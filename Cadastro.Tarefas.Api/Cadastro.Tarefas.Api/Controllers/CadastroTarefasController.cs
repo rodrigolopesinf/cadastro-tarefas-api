c0d7e381b7da8dbaae339df07b0c2a28c01daf25
rodrigo.silva <rodrigo.silva@SL061>
2022-08-18 19:36:18 -0300

    Criação do projeto, definição da arquitetura e criação das classe de modelo

---
+++ Cadastro.Tarefas.Api/Cadastro.Tarefas.Api/Controllers/CadastroTarefasController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Cadastro.Tarefas.Api.Controllers
+{
+    public class CadastroTarefasController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
